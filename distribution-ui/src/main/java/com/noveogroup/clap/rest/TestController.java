e5b7939d683b03ff62efe79275095ca5ee224620
Ragnar <ragnar.nsk@gmail.com>
2013-05-08 20:17:08 +0700

    json serialization added

--- distribution-ui/src/main/java/com/noveogroup/clap/rest/TestController.java
+++ distribution-ui/src/main/java/com/noveogroup/clap/rest/TestController.java
@@ -2,6 +2,7 @@ package com.noveogroup.clap.rest;
 
 import com.noveogroup.ProjectService;
 import com.noveogroup.clap.entity.Project;
+import com.noveogroup.clap.model.ProjectDTO;
 import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 
@@ -9,7 +10,9 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @ApplicationScoped
 @Path("/")
@@ -26,5 +29,14 @@ public class TestController {
         Project project = new Project();
         project.setName("Name");
         return MAPPER.map(project, com.noveogroup.clap.model.ProjectDTO.class).getName();
+    }
+
+    @GET
+    @Path("testProject")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ProjectDTO getTestProject(@QueryParam("q") String name) {
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setName(name);
+        return projectDTO;
     }
 }
