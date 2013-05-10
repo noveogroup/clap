e499728e64447eca16120ec6c5629e93b888c4f6
Ragnar <ragnar.nsk@gmail.com>
2013-05-10 15:56:19 +0700

    qrcode generation support added

--- distribution-ui/src/main/java/com/noveogroup/clap/web/HelloBean.java
+++ distribution-ui/src/main/java/com/noveogroup/clap/web/HelloBean.java
@@ -1,20 +1,31 @@
 package com.noveogroup.clap.web;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Writer;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.noveogroup.clap.service.ProjectService;
 
 import com.noveogroup.clap.web.util.config.ConfigBean;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ejb.Stateless;
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * @author
  */
 @Named
-@Stateless
+@RequestScoped
 public class HelloBean {
 
     private static final Logger LOG = LoggerFactory.getLogger(HelloBean.class);
@@ -25,6 +36,16 @@ public class HelloBean {
     @Inject
     private ConfigBean configBean;
 
+    private StreamedContent QRCode;
+
+    public HelloBean() throws IOException, WriterException {
+        File qrcodeFile = new File("QRCode");
+        Writer writer = new QRCodeWriter();
+        BitMatrix matrix = writer.encode("ololo-test", BarcodeFormat.QR_CODE, 100, 100);
+        MatrixToImageWriter.writeToFile(matrix,"PNG",qrcodeFile);
+        QRCode = new DefaultStreamedContent(new FileInputStream(qrcodeFile), "image/png");
+    }
+
     public String getGreeting() {
         LOG.trace("Hello World!");
         LOG.debug("How are you today?");
@@ -32,5 +53,9 @@ public class HelloBean {
         LOG.warn("I love programming.");
         LOG.error("I am programming.");
         return "Hello World!" + projectService.getName()+" " + configBean.getTestProperty();
+    }
+
+    public StreamedContent getQRCode() {
+        return QRCode;
     }
 }
