f2425915db8ab4882bdce2bdcad104cfed96040b
Ragnar <ragnar.nsk@gmail.com>
2013-05-25 23:50:22 +0700

    Я, блять, ненавижу ентерпрайз!!!!!!

--- clap-server/clap-server-ui-core/src/main/java/com/noveogroup/clap/web/util/init/DBInitBean.java
+++ clap-server/clap-server-ui-core/src/main/java/com/noveogroup/clap/web/util/init/DBInitBean.java
@@ -5,6 +5,7 @@ import com.noveogroup.clap.model.Project;
 import com.noveogroup.clap.model.auth.Authentication;
 import com.noveogroup.clap.model.revision.Revision;
 import com.noveogroup.clap.model.user.User;
+import com.noveogroup.clap.service.init.InitService;
 import com.noveogroup.clap.service.project.ProjectService;
 import com.noveogroup.clap.service.revision.RevisionService;
 import com.noveogroup.clap.model.request.revision.AddOrGetRevisionRequest;
@@ -22,49 +23,12 @@ import java.util.Date;
 @ApplicationScoped
 public class DBInitBean {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(DBInitBean.class);
-
     @Inject
-    private ProjectService projectService;
-
-    @Inject
-    private RevisionService revisionService;
-
-    @Inject
-    private UserService userService;
-
-    @Inject
-    private AuthenticationRequestHelper authenticationRequestHelper;
+    private InitService initService;
 
     @PostConstruct
     protected void initDB(){
-
-        User user = new User();
-        user.setLogin("test");
-        user.setPassword("123");
-        user = userService.createUser(user);
-        LOGGER.debug("user created = " + user);
-
-        final Authentication authentication = new Authentication();
-        authentication.setUser(user);
-        authenticationRequestHelper.applyAuthentication(authentication);
-
-        Project project = new Project();
-        project.setName("test_project");
-        project.setExternalId("test_ext_id");
-        project.setCreationDate(new Date(1368308474));
-        project = projectService.createProject(project);
-
-        LOGGER.debug("project created : " + project);
-
-        Revision revision = new Revision();
-        revision.setTimestamp(1368318776L);
-        final AddOrGetRevisionRequest request = new AddOrGetRevisionRequest();
-        request.setProjectExternalId(project.getExternalId());
-        request.setRevision(revision);
-        revision = revisionService.addOrGetRevision(request);
-
-        LOGGER.debug("revision created : " + revision);
+        initService.initDB();
     }
 
     public void fakeMethod(){
