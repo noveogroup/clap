565cab44c8d65a8e64b25f0baa58768f42a37138
Pavel Stepanov <stefan.nsk@gmail.com>
2014-08-12 16:39:31 +0700

    fix test application

---
+++ clap-test/Application/src/main/java/com/noveogroup/clap/test/TestActivity.java
@@ -0,0 +1,40 @@
+package com.noveogroup.clap.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.noveogroup.clap.api.Trace;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class TestActivity extends Activity {
+
+    @Trace
+    private String test(String input) {
+        return String.format("[%s]", input);
+    }
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+        findViewById(R.id.button_crash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                throw new RuntimeException("test crash");
+            }
+        });
+
+        findViewById(R.id.button_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                test(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+            }
+        });
+    }
+
+}
