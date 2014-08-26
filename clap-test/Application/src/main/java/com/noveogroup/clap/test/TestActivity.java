af18a2dd1d9e63ab1e315566abaa3c8258c9e9e2
Pavel Stepanov <stefan.nsk@gmail.com>
2014-08-26 14:31:57 +0700

    test logs

--- clap-test/Application/src/main/java/com/noveogroup/clap/test/TestActivity.java
+++ clap-test/Application/src/main/java/com/noveogroup/clap/test/TestActivity.java
@@ -2,6 +2,7 @@ package com.noveogroup.clap.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,6 +11,24 @@ import java.util.Date;
 
 // todo uncomment logic when clap-api will be introduced
 public class TestActivity extends Activity {
+
+    private static void log() {
+        Log.v("LOG", "v(String, String)");
+        Log.v("LOG", "v(String, String, Throwable)", new Exception());
+        Log.d("LOG", "d(String, String)");
+        Log.d("LOG", "d(String, String, Throwable)", new Exception());
+        Log.i("LOG", "i(String, String)");
+        Log.i("LOG", "i(String, String, Throwable)", new Exception());
+        Log.w("LOG", "w(String, String)");
+        Log.w("LOG", new Exception("w(String, Throwable)"));
+        Log.w("LOG", "w(String, String, Throwable)", new Exception());
+        Log.e("LOG", "e(String, String)");
+        Log.e("LOG", "e(String, String, Throwable)", new Exception());
+        Log.wtf("LOG", "wtf(String, String)");
+        Log.wtf("LOG", new Exception("wtf(String, Throwable)"));
+        Log.wtf("LOG", "wtf(String, String, Throwable)", new Exception());
+        Log.println(Log.DEBUG, "LOG", "println(int, String, String)");
+    }
 
     // @Trace
     private String test(String input) {
@@ -25,6 +44,7 @@ public class TestActivity extends Activity {
         findViewById(R.id.button_crash).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                log();
                 throw new RuntimeException("test crash");
             }
         });
