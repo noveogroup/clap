9c7ed4a64c5b26979740311d2d9355c8b84a8336
Pavel Stepanov <stefan.nsk@gmail.com>
2014-09-01 15:29:43 +0700

    decrease logcat size

--- clap-plugin/android-libraries/module-crashSpy/src/main/java/com/noveogroup/clap/module/crash_spy/LogHelper.java
+++ clap-plugin/android-libraries/module-crashSpy/src/main/java/com/noveogroup/clap/module/crash_spy/LogHelper.java
@@ -42,7 +42,7 @@ public final class LogHelper {
         throw new UnsupportedOperationException();
     }
 
-    private static final int MAX_LOG_SIZE = 1000;
+    private static final int MAX_LOG_SIZE = 250;
 
     public static List<String> getLogCat() {
         try {
