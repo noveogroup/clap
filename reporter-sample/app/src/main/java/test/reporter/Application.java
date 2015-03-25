1cbe1d27b33513f8c36e74fae8fceb289d1b5119
Pavel Stepanov <stefan.nsk@gmail.com>
2015-03-25 18:58:09 +0600

    delete slf4j and logback

--- reporter-sample/app/src/main/java/test/reporter/Application.java
+++ reporter-sample/app/src/main/java/test/reporter/Application.java
@@ -2,18 +2,12 @@ package test.reporter;
 
 import com.noveogroup.android.reporter.library.Reporter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class Application extends android.app.Application {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
     @Override
     public void onCreate() {
         super.onCreate();
         Reporter.initContext(this);
-        LOGGER.info("Application::onCreate");
     }
 
 }
--- reporter-sample/app/src/main/java/test/reporter/MainActivity.java
+++ reporter-sample/app/src/main/java/test/reporter/MainActivity.java
@@ -3,34 +3,21 @@ package test.reporter;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-import com.noveogroup.android.reporter.library.Log;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import example.AndroidLibrary;
-import example.JavaLibrary;
+import com.noveogroup.android.reporter.library.Logger;
+import com.noveogroup.android.reporter.library.Reporter;
 
 public class MainActivity extends Activity {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainActivity.class);
+    private static final Logger logger = Reporter.getLogger();
+    private static final String TAG = "ReporterSample";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        LOGGER.info("MainActivity::onCreate");
-        LOGGER.info("AndroidLibrary.process: {}", AndroidLibrary.process(this));
-        LOGGER.info("JavaLibrary.process: {}", JavaLibrary.process(this));
-
-        findViewById(R.id.main_crash).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                throw new RuntimeException("unexpected exception");
-            }
-        });
 
         findViewById(R.id.main_system_crash).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,21 +29,21 @@ public class MainActivity extends Activity {
         findViewById(R.id.main_system_logcat).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LOGGER.info(String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
+                Log.i(TAG, String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
             }
         });
 
         findViewById(R.id.main_reporter_crash).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.crash(new RuntimeException("unexpected exception"));
+                logger.crash(new RuntimeException("unexpected exception"));
             }
         });
 
         findViewById(R.id.main_reporter_log).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i(String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
+                logger.i(String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
             }
         });
 
@@ -66,7 +53,7 @@ public class MainActivity extends Activity {
                 View view = getWindow().getDecorView();
                 view.setDrawingCacheEnabled(true);
                 Bitmap bitmap = view.getDrawingCache();
-                Log.image(bitmap);
+                logger.image(bitmap);
             }
         });
     }
