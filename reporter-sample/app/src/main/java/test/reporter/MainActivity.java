21394202294a0ad5d0991da592359348dae20e32
Pavel Stepanov <stefan.nsk@gmail.com>
2015-03-25 18:53:24 +0600

    improve sample: add more useful buttons

--- reporter-sample/app/src/main/java/test/reporter/MainActivity.java
+++ reporter-sample/app/src/main/java/test/reporter/MainActivity.java
@@ -1,6 +1,7 @@
 package test.reporter;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 
@@ -24,18 +25,48 @@ public class MainActivity extends Activity {
         LOGGER.info("AndroidLibrary.process: {}", AndroidLibrary.process(this));
         LOGGER.info("JavaLibrary.process: {}", JavaLibrary.process(this));
 
-        findViewById(R.id.main_log).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
-                LOGGER.info(String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
-            }
-        });
-
         findViewById(R.id.main_crash).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 throw new RuntimeException("unexpected exception");
+            }
+        });
+
+        findViewById(R.id.main_system_crash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                throw new RuntimeException("unexpected exception");
+            }
+        });
+
+        findViewById(R.id.main_system_logcat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LOGGER.info(String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
+            }
+        });
+
+        findViewById(R.id.main_reporter_crash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.crash(new RuntimeException("unexpected exception"));
+            }
+        });
+
+        findViewById(R.id.main_reporter_log).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(String.format("now is %1$tF %1$tT", System.currentTimeMillis()));
+            }
+        });
+
+        findViewById(R.id.main_reporter_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View view = getWindow().getDecorView();
+                view.setDrawingCacheEnabled(true);
+                Bitmap bitmap = view.getDrawingCache();
+                Log.image(bitmap);
             }
         });
     }
